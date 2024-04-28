cf607b7429f7e7fca3706057526c5bf6244dc55f
Kai Svenson <ksvenson@caltech.edu>
2024-04-28 00:59:40 -0700

    saving changes

--- lab2/main.py
+++ lab2/main.py
@@ -13,7 +13,7 @@ FIGS_DIR = './figs/'
 EIGS_DIR = '../lab1/data/'
 
 HSPACE = np.linspace(0, 2, 17)
-LSPACE = np.arange(5, 21)
+LSPACE = np.arange(5, 13)
 
 LEGEND_OPTIONS = {'bbox_to_anchor': (0.9, 0.5), 'loc': 'center left'}
 FIG_SAVE_OPTIONS = {'bbox_inches': 'tight'}
@@ -46,6 +46,11 @@ def get_entropy(states, l, note=None):
     probs = svd['s']**2
     return -np.sum(probs * np.log(probs), axis=-1)
 
+
+def entropy_fit(L):
+    def fit(l, scale, offset):
+        return (scale / 3) * np.log((L / np.pi) * np.sin(np.pi * l / L)) + offset
+    return fit
 
 
 def p5_1():
@@ -83,37 +88,72 @@ def p5_2():
     }
     fig = plt.figure(figsize=(15, 10))
     subfigs = fig.subfigures(nrows=2, ncols=1)
+    entropy = {}
     for bdry_idx, bdry in enumerate(('open', 'loop')):
+        entropy[bdry] = {}
         axes = subfigs[bdry_idx].subplots(nrows=1, ncols=3, sharex=True, sharey=True)
-        entropy = {}
         for L in LSPACE:
             data = np.load(EIGS_DIR + f'sparse_eigs_{bdry}_L{L}.npz')
-            entropy[L] = {}
+            entropy[bdry][L] = {}
             for i, phase in enumerate(phase_h):
-                entropy[L][phase] = []
+                entropy[bdry][L][phase] = []
                 gnd_states = data['evecs'][:, :, 0]
                 for l in np.arange(1, L):
                     print(f'getting entropy {bdry} L={L} l={l}')
-                    entropy[L][phase].append(get_entropy(gnd_states, l, note=f'gnd_state_{bdry}_L{L}_l{l}')[phase_h[phase]])
+                    entropy[bdry][L][phase].append(get_entropy(gnd_states, l, note=f'gnd_state_{bdry}_L{L}_l{l}')[phase_h[phase]])
 
-                axes[i].plot(np.arange(1, L), entropy[L][phase], label=rf'$L={L}$')
+                axes[i].plot(np.arange(1, L), entropy[bdry][L][phase], label=rf'$L={L}$')
                 axes[i].set_ylim([0, 1])
                 if bdry_idx == 0:
                     axes[i].set_title(phase + rf': $h/J={HSPACE[phase_h[phase]]}$')
                 if bdry_idx == 1:
                     axes[i].set_xlabel(r'$\ell$')
                 handles, labels = axes[0].get_legend_handles_labels()
-        axes[0].set_ylabel(r'$S$')
+        axes[0].set_ylabel('Entanglement Entropy')
 
     subfigs[0].suptitle(f'Open Boundary')
     subfigs[1].suptitle(f'Periodic Boundary')
-
     fig.legend(handles, labels, **LEGEND_OPTIONS)
     fig.savefig(FIGS_DIR + 'entropy.png', **FIG_SAVE_OPTIONS)
 
+    summary = {}
+    for bdry in entropy:
+        summary[bdry] = {}
+        for phase in phase_h:
+            summary[bdry][phase] = []
+            for L in entropy[bdry]:
+                summary[bdry][phase].append(entropy[bdry][L][phase][L // 2])
 
+    fig = plt.figure(figsize=(15, 10))
+    subfigs = fig.subfigures(nrows=2, ncols=1)
+    for bdry_idx, bdry in enumerate(summary):
+        axes = subfigs[bdry_idx].subplots(nrows=1, ncols=3, sharex=True, sharey=True)
+        for i, phase in enumerate(summary[bdry]):
+            axes[i].plot(LSPACE, summary[bdry][phase])
+            if bdry_idx == 0:
+                axes[i].set_title(phase + rf': $h/J={HSPACE[phase_h[phase]]}$')
+            if bdry_idx == 1:
+                axes[i].set_xlabel(r'$L$')
+        axes[0].set_ylabel(r'$S(L/2, L)$')
 
+    subfigs[0].suptitle(f'Open Boundary')
+    subfigs[1].suptitle(f'Periodic Boundary')
+    fig.savefig(FIGS_DIR + 'entropy_summary.png', **FIG_SAVE_OPTIONS)
 
+    L_max = LSPACE[-1]
+    fit_data = entropy['loop'][L_max]['Critical Point']
+    popt, pcov = sp.optimize.curve_fit(entropy_fit(L_max), np.arange(1, L_max), fit_data, p0=(1, 0))
+    std = np.sqrt(np.diag(pcov))
+    with open('p5_2_fit_results.txt', 'w') as f:
+        print(f'scale: {round(popt[0], 5)} \pm {round(std[0], 3)}', file=f)
+        print(f'offset: {round(popt[1], 5)} \pm {round(std[1], 3)}', file=f)
+    plt.figure()
+    plt.plot(np.arange(1, L_max), fit_data, label='Measured E.E.')
+    plt.plot(np.arange(1, L_max), entropy_fit(L_max)(np.arange(1, L_max), *popt), label='Equation 26 Fit')
+    plt.xlabel(r'$\ell$')
+    plt.ylabel('Entanglement Entropy')
+    plt.legend()
+    plt.savefig(FIGS_DIR + 'entropy_fit.png', **FIG_SAVE_OPTIONS)
 
 
 
