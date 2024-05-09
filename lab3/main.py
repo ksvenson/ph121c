120770d247094516ea000b5a4151b1aa8cd0470a
Kai Svenson <ksvenson@caltech.edu>
2024-05-09 14:24:01 -0700

    made and diagonalized hamiltonian

--- lab3/main.py
+++ lab3/main.py
@@ -7,9 +7,13 @@ import utility
 
 CACHE_DIR = './data/'
 
+LSPACE = np.arange(8, 15)
 
-@utility.cache('npz', CACHE_DIR + 'dense_H')
-def make_dense_H(L, hx=-1.05, hz=0.5, note=None):
+FIELD_VALS = {'hx': -1.05, 'hz': 0.5}
+
+
+@utility.cache('npy', CACHE_DIR + 'dense_H')
+def make_dense_H(L, note=None):
     """
     Makes Hamiltonian in sigma_z basis as defined in Equation 8 with periodic boundary conditions.
     States are indexed in binary: 0 for spin down and 1 for spin up.
@@ -17,26 +21,32 @@ def make_dense_H(L, hx=-1.05, hz=0.5, note=None):
     :param note: appeneded to file name when caching function output.
     :return: Hamiltonian with shape (2**L, 2**L).
     """
-    states = np.arange(2**L)
-    # Perform a XOR between states, and states shifted by 1 bit. Make sure that last bit is set to zero.
-    bulk = 2 * ((states & ~(1 << L-1)) ^ (states >> 1)).bit_count() - (L-1)
-    # Include the periodic term.
-    loop = 2 * ((states ^ (states >> (L - 1))) % 2) - 1
-    # h_z field
-    z_field = 2 * states.bit_count() - 1
-    H = np.diag(bulk + loop)
-    for i in range(dim):
-        H_open[i, i] = 2 * ((i & ~(1 << L-1)) ^ (i >> 1)).bit_count() - (L-1)
-        for flip in range(0, L):
-            H_open[:, i ^ (1 << flip), i] -= hspace
-        H_loop[:, :, i] = H_open[:, :, i]
-        H_loop[:, i, i] += 2 * ((i ^ (i >> (L - 1))) % 2) - 1
-    return {'open': H_open, 'loop': H_loop}
+    H = np.zeros((2**L, 2**L))
+    for i in range(2**L):
+        # Perform a XOR between states, and states shifted by 1 bit. Make sure that last bit is set to zero.
+        H[i, i] = 2 * ((i & ~(1 << L-1)) ^ (i >> 1)).bit_count() - (L-1)
+        # Include the periodic term.
+        H[i, i] += 2 * ((i ^ (i >> (L - 1))) % 2) - 1
+        # h_z field
+        H[i, i] += -1 * FIELD_VALS['hz'] * (2 * i.bit_count() - L)
+        # h_x field
+        flips = np.arange(L)
+        H[i ^ (1 << flips), i] += -1 * FIELD_VALS['hx']
+    return H
+
+
+@utility.cache('npz', CACHE_DIR + 'l3_dense_eigs')
+def dense_eigs(L, note=None):
+    print(f'finding dense evals: L={L}')
+    H = make_dense_H(L, note=note)
+    evals, evecs = sp.linalg.eigh(H)
+    return {'evals': evals, 'evecs': evecs}
 
 
 def p4_1():
-    pass
+    for L in LSPACE:
+        dense_eigs(L, note=f'L{L}')
 
 
 if __name__ == '__main__':
-    pass
+    p4_1()
