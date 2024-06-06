83a5466c495c9850bf06379642b8c9376123fab2
Kai Svenson <ksvenson@caltech.edu>
2024-06-06 01:54:56 -0700

    fixed hamiltonian with random W

--- lab3/main.py
+++ lab3/main.py
@@ -25,17 +25,18 @@ ops = {
 }
 
 
-@utility.cache('npy', CACHE_DIR + 'l3_dense_H')
 def make_dense_H(L, W=None, note=None):
     """
     Makes Hamiltonian in sigma_z basis as defined in Equation 8 and 11 with periodic boundary conditions.
     States are indexed in binary: 0 for spin down and 1 for spin up.
     :param L: System size.
     :param W: Uniform distribution parameter.
-    :param note: Appeneded to filename when caching function output.
+    :param note: Appended to filename when caching function output.
     :return: Hamiltonian with shape (2**L, 2**L).
     """
     H = np.zeros((2**L, 2**L))
+    hx = np.random.uniform(low=-W, high=W, size=L)
+    hz = np.random.uniform(low=-W, high=W, size=L)
     for i in range(2**L):
         # Perform a XOR between states, and states shifted by 1 bit.
         H[i, i] += 2 * (i ^ cycle_bits(i, L, 1)).bit_count() - L
@@ -48,9 +49,6 @@ def make_dense_H(L, W=None, note=None):
             # h_x field
             H[i ^ (1 << flips), i] += -1 * FIELD_VALS['hx']
         else:
-            hx = np.random.uniform(low=-W, high=W, size=L)
-            hz = np.random.uniform(low=-W, high=W, size=L)
-
             # h_z field
             sigma_z = 2 * np.array([int(bit) for bit in format(i, f'0{L}b')]) - 1
             H[i, i] += -1 * np.sum(hz * sigma_z)
@@ -181,84 +179,6 @@ def make_trans_op(L):
     states = np.arange(2**L)
     T[cycle_bits(states, L, 1), states] = 1
     return T
-
-
-# def ham_analysis(prob, W=None):
-#     """
-#     Makes all necessary plots for 4.1.1., 4.1.2, and 4.3.1. Leave `W=None` for translation symmetry.
-#     :param prob: A tag used to name figures.
-#     :param W: Uniform distribution parameter
-#     :return: Nothing. Saves plots to `FIGS_DIR`.
-#     """
-#     fig_sig, axes_sig = plt.subplots(3, 1, sharex=True, sharey=True, figsize=(15, 15))
-#     fig_beta, axes_beta = plt.subplots(figsize=(5, 5))
-#     fig_entropy, axes_entropy = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(10, 5))
-#     for color_idx, L in enumerate(LSPACE):
-#         print(f'L={L}')
-#         eigs = dense_eigs(L, W=W, note=f'{prob}_L{L}')
-#         evals = eigs['evals']
-#         evecs = eigs['evecs']
-#
-#         xi_state = make_prod_state(L, 1/2, -np.sqrt(3)/2)
-#         # convert xi state to diagonal basis
-#         xi_state = evecs.T.conj() @ xi_state
-#
-#         propagator = np.exp(-1j * np.multiply.outer(TSPACE, evals))
-#         # This method directly evolves the state like in Equation 2, which I found to be faster than
-#         # evolving the expectation value, like in Equation 3.
-#         evolved_states = (propagator * xi_state).T
-#
-#         # Thermal calculations
-#         Z_beta = np.array([np.sum(np.exp(-beta * evals)) for beta in beta_space])
-#         E_beta = np.array([np.sum(np.exp(-beta * evals) * evals) for beta in beta_space]) / Z_beta
-#         xi_eng = np.sum(xi_state.conj() * evals * xi_state)
-#         xi_beta_idx = np.argmin(np.abs(E_beta - xi_eng))
-#         xi_beta = beta_space[xi_beta_idx]
-#
-#         for op_idx, op in enumerate(ops):
-#             print(f'op: {op}')
-#             global_op = rebase_operator(L, ops[op], evecs)
-#             measurement = np.sum(evolved_states.conj() * (global_op @ evolved_states), axis=0)
-#
-#             O_thermal = np.sum(np.diag(global_op) * np.exp(-1 * xi_beta * evals)) / Z_beta[xi_beta_idx]
-#
-#             axes_sig[op_idx].plot(TSPACE, measurement, label=f'$L={L}$', color=f'C{color_idx}')
-#             axes_sig[op_idx].axhline(O_thermal, label=rf'$L={L}$ Thermal Limit', color=f'C{color_idx}', linestyle='dotted')
-#             axes_sig[op_idx].set_ylabel(rf'$\langle \sigma_1^{op}(t) \rangle$')
-#             axes_sig[op_idx].set_title(rf'$\mu={op}$')
-#         axes_beta.plot(beta_space, E_beta, label=rf'$L={L}$')
-#
-#         # Entropy calculations
-#         my_state = make_prod_state(L, -1/np.sqrt(2), 1/np.sqrt(2))
-#         my_state = evecs.T.conj() @ my_state
-#         my_evolved_states = (propagator * my_state).T
-#         all_states = {'xi': evolved_states, 'my': my_evolved_states}
-#         ax_idx = 0
-#         for tag, states in all_states.items():
-#             # convert states back to sigma z basis:
-#             sigz_states = evecs @ states
-#             entropy = l2main.get_entropy(sigz_states.T, L//2, note=f'{prob}_L{L}_{tag}_entropy_trace')
-#
-#             axes_entropy[ax_idx].plot(TSPACE, entropy, label=rf'$L={L}$')
-#             axes_entropy[ax_idx].set_xlabel(r'Time $t$')
-#             ax_idx += 1
-#
-#     axes_sig[-1].set_xlabel('Time $t$')
-#     handles, labels = axes_sig[0].get_legend_handles_labels()
-#     fig_sig.legend(handles, labels, **LEGEND_OPTIONS)
-#     fig_sig.savefig(FIGS_DIR + f'{prob}_sigma.png', **FIG_SAVE_OPTIONS)
-#
-#     axes_beta.set_xlabel(r'$\beta$')
-#     axes_beta.set_ylabel(r'$E_\beta$')
-#     fig_beta.legend(**LEGEND_OPTIONS)
-#     fig_beta.savefig(FIGS_DIR + f'{prob}_E_beta.png', **FIG_SAVE_OPTIONS)
-#
-#     axes_entropy[0].set_title(r'$\otimes \frac{1}{2}(|\uparrow\rangle - \sqrt{3}|\downarrow\rangle)$')
-#     axes_entropy[1].set_title(r'$\otimes \frac{1}{\sqrt{2}}(-|\uparrow\rangle + |\downarrow\rangle)$')
-#     axes_entropy[0].set_ylabel(r'$S_{L/2}(t)$')
-#     handles, labels = axes_entropy[0].get_legend_handles_labels()
-#     fig_entropy.legend(handles, labels, **LEGEND_OPTIONS)
-#     fig_entropy.savefig(FIGS_DIR + f'{prob}_entropy.png', **FIG_SAVE_OPTIONS)
 
 
 def do_ham_analysis(prob, W=None):
@@ -408,7 +328,7 @@ def p4_2_12():
 
 
 def p4_3_1():
-    for W in (10,):
+    for W in (3, 10):
         data = []
         trials = np.arange(10)
         for trial in trials:
@@ -431,7 +351,7 @@ def p4_3_1():
             for tag in data[0][L]['entropy']:
                 avg[L]['entropy'][tag] = np.mean([data[i][L]['entropy'][tag] for i in trials], axis=0)
 
-        plot_ham_analysis(f'p4_2_1_W{W}_avg', avg)
+        plot_ham_analysis(f'p4_3_1_W{W}_avg', avg)
 
 
 def p4_3_2(Omega=1):
@@ -467,10 +387,10 @@ def p4_3_2(Omega=1):
 if __name__ == '__main__':
     np.random.seed(628)  # tau = 2 pi - the better circle constant!
 
-    # p4_1_123()
+    p4_1_123()
 
-    # p4_2_12()
+    p4_2_12()
 
-    # p4_3_1()
+    p4_3_1()
 
     p4_3_2()
